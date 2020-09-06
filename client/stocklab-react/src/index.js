618f654a918df670b40f76e10e0e88552122012f
raccoonjeong <rana3782@gmail.com>
2020-09-06 20:12:08 +0900

    7장 252page까지

--- client/stocklab-react/src/index.js
+++ client/stocklab-react/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CodeInfo from './pages/CodeInfo';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<CodeInfo/>,
   document.getElementById('root')
 );
 
--- client/stocklab-react/src/pages/CodeInfo.js
+++ client/stocklab-react/src/pages/CodeInfo.js
@@ -3,8 +3,8 @@ import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import CodeSearch from '../components/CodeSearch';
-import CodePrice from '../components/CodePrice';
-import CodeChart from '../components/CodeChart';
+// import CodePrice from '../components/CodePrice';
+// import CodeChart from '../components/CodeChart';
 
 const styles = {
     root: {
@@ -34,6 +34,7 @@ class CodeInfo extends Component {
         this.setState({selectedCode});
     }
     render() {
+        const {classes} = this.props;
         return (
             <div>
                 <div>
